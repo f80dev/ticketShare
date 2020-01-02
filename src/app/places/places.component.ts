55f020fab8ee24d6a361fb019f6242b046c4261e
f80dev <rv@f80.fr>
2020-01-02 12:56:33 +0100

    first commit for beta version

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -11,16 +11,27 @@ import {ConfigService} from "../config.service";
 export class PlacesComponent implements OnInit {
 
   tickets:any[]=[];
+  hourglass=false;
 
-  constructor(public api: ApiService,public config:ConfigService,
-              public route: ActivatedRoute) { }
+  constructor(public api: ApiService,
+              public config:ConfigService,
+              public route: ActivatedRoute) {
+
+  }
 
 
-  ngOnInit() {
+  refresh(){
+    this.hourglass=true;
     var params:ParamMap=this.route.snapshot.queryParamMap;
     this.api.available(params.get("event"),localStorage.getItem("address")).subscribe((r:any)=>{
-      this.tickets=r;
+      this.hourglass=false;
+      if(r!=null)
+        this.tickets=r;
     });
+  }
+
+  ngOnInit() {
+    this.refresh();
   }
 
 }
