ea673727d7c6fd5a5f7330f50380d4af2d9d6b2f
f80dev <rv@f80.fr>
2020-02-06 10:08:23 +0100

    first commit for beta version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  domain_appli:"https://app.kerberus.fr",
+  domain_appli:"https://app.kerberus.tech",
   root_api: 'https://server.f80.fr:6800/api',
   socket_server: "https://server.f80.fr:6800",
   config_file:"./assets/config.json"
