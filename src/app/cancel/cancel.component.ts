8af24eb0cce40577bda2a41c6c5bcf620fb4be0b
f80dev <rv@f80.fr>
2020-05-13 00:25:26 +0200

    Ajout du système de requete

---
+++ src/app/cancel/cancel.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {ConfigService} from "../config.service";
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material";
+import {ClipboardService} from "ngx-clipboard";
+import {Location} from "@angular/common";
+import {ApiService} from "../api.service";
+import {checkLogin, showMessage} from "../tools";
+
+@Component({
+  selector: 'app-cancel',
+  templateUrl: './cancel.component.html',
+  styleUrls: ['./cancel.component.sass']
+})
+export class CancelComponent implements OnInit {
+
+  _event:any={};
+  message="";
+
+  constructor(
+    public config:ConfigService,
+    public router:Router,
+    public toast:MatSnackBar,
+    public clipboard:ClipboardService,
+    public route:ActivatedRoute,
+    public _location:Location,
+    public api:ApiService
+  ) {
+
+  }
+
+  ngOnInit() {
+    checkLogin(this.router);
+    var params:ParamMap=this.route.snapshot.queryParamMap;
+    if(this.config.user){
+      this.message="Chargement de l'événement";
+      this.config.reload_user(()=>{
+        this.message="";
+        this._event=this.config.user._events[params.get("idevent")];
+      })
+    }
+  }
+
+  cancel_event(auto){
+    this.message="Annulation de l'événement";
+    this.api.delevent(this._event._id,auto,this._event.refund_address).subscribe(()=>{
+      this.message="";
+      this._location.back();
+    });
+  }
+
+  to_draft(){
+    this.api.setevent(this._event._id,{state:"draft"}).subscribe(()=>{
+      showMessage(this,"Annulation de la demande de mise en ligne");
+      this._location.back();
+    });
+  }
+
+
+
+}
